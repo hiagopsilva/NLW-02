3971c6487314c4cf6ed594f4592c67addfe2c41f
hiagopsilva <hiagohps360@gmail.com>
2020-08-06 22:04:46 -0300

    update routes

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from '../pages/Landing';
+import TeacherList from '../pages/TeacherList';
+import TeacherForm from '../pages/TeacherForm';
 
 const Routes: React.FC = () => (
-  <Switch>
-    <Route path="/" exact component={Landing} />
-  </Switch>
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Landing} />
+      <Route path="/study" component={TeacherList} />
+      <Route path="/give-classes" component={TeacherForm} />
+    </Switch>
+  </BrowserRouter>
 );
 
 export default Routes;
